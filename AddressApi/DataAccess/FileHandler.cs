490c913214dd94e207ccd289a02c283639ee4662
mobocomputer <mobocomputer@TEST-SYSTEM>
2023-12-22 10:31:29 -0800

    I created a class to work with file as database.

--- AddressApi/DataAccess/FileHandler.cs
+++ AddressApi/DataAccess/FileHandler.cs
@@ -1,6 +1,24 @@
-﻿namespace AddressApi.DataAccess
+﻿using System.IO;
+
+namespace AddressApi.DataAccess
 {
     public class FileHandler
     {
+        private string _filePath;
+
+        public FileHandler(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void SaveToFile(string data)
+        {
+            File.AppendAllText(_filePath, data + Environment.NewLine);
+        }
+
+        public string[] ReadFile()
+        {
+            return File.ReadAllLines(_filePath);
+        }
     }
 }
